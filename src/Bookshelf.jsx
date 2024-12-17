cc25fc3806331ffddd7927d109c78706193498a4
NikolaosG <Nikolaos_Gkazgkas@outlook.com>
2024-12-17 22:19:09 +0000

    Controlled forms

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -1,11 +1,20 @@
+
 import { useState } from 'react';
 
-// const [books, setBooks] = useState([
-//     { title: 'Fourth Wing', author: 'Rebecca Yarros' },
-//     { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
-//   ]);
+
 
 const Bookshelf = () => {
+  
+  const [books, setBooks] = useState([
+    { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+    { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+  ]);
+  
+  const [newBook, setNewBook] = useState (
+    {title: '', author: ''},
+  );
+  
+  return (
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
@@ -13,6 +22,6 @@ const Bookshelf = () => {
   </div>
   <div className="bookCardsDiv">{/* Book cards will display here */}</div>
 </div>
-}
+)};
 
-export default Bookshelf
+export default Bookshelf;
