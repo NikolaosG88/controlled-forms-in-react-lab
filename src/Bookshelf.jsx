9e06c918699cbda6861ca0c0c57bc3e4794a2288
NikolaosG <Nikolaos_Gkazgkas@outlook.com>
2024-12-18 21:13:05 +0000

    Controlled Forms book shelf lab

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -1,11 +1,6 @@
 
 import { useState } from 'react';
 
-const handleInputChange = (event) => {
-  //  event.preventDefault();
-   setNewBook({ ...newBook, [event.target.name]: event.target.value });
-
-};
 
 const Bookshelf = () => {
   
@@ -17,13 +12,26 @@ const Bookshelf = () => {
   const [newBook, setNewBook] = useState (
     {title: '', author: ''},
   );
+  
+  const handleInputChange = (event) => {
+    //  event.preventDefault();
+     setNewBook({ ...newBook, [event.target.name]: event.target.value });
+  
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    setBooks([...books, newBook]);
+    setNewBook({ title: '', author: '' });
+    };
 
   return (
     <>
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
-  
+    <form onSubmit={handleSubmit}>
     <label htmlFor="title">Title: </label>
           <input
             id="title"
@@ -31,7 +39,7 @@ const Bookshelf = () => {
             value={newBook.title}
             onChange={handleInputChange}
           />
-          <label htmlFor="title">Author: </label>
+          <label htmlFor="author">Author: </label>
           <input
             id="author"
             name="author"
@@ -39,7 +47,9 @@ const Bookshelf = () => {
             onChange={handleInputChange}
           />
         <button type="submit">Add  Book</button>
+        </form>
   </div>
+  
   <div className="bookCardsDiv">
     {/* Book cards will display here */}
     {books.map((book, index) => (
